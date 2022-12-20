ddaff671cac682f362751e2c691ddf9baa2f1567
lucasr98 <lucascalavera@hotmail.com>
2022-12-20 18:50:11 -0300

    20/12/22 (Agregado de función next/prev image, y máscara de imágenes)

--- index.js
+++ index.js
@@ -5,16 +5,20 @@ fulImg = document.getElementById("fulImg"),
 imgContainer = document.getElementById("imgContainer");
 const header = document.getElementById("header");
 const x = document.getElementById("x");
-const imagenes = document.querySelectorAll(".grid-item__item")
+const imagenes = document.querySelectorAll(".grid-item__item");
+const modalButtons = document.querySelectorAll(".modal__buttons");
 
 let contador = 0;
 
 // IMAGEN MODAL
 
 function openFulImg(reference){
-	fulImgBox.style.display = "flex"
+	fulImgBox.style.display = "flex";
 	fulImg.src = reference;
-	x.style.display = "flex"
+	x.style.display = "flex";
+	modalButtons.forEach(button =>{
+		button.style.display = "flex";
+	})
 	disableScroll();
 }
 
@@ -42,20 +46,19 @@ function fulImgClose(e){
 		return;
 	}
 	else{
-		fulImg.classList.replace("img_ampliada", "img_original");
-		fulImgBox.classList.replace("ful_img_ampliado", "ful_img");
-		imgContainer.classList.replace("img_container_ampliado", "img_container");
-		contador = 0;
 		closeImg();
 	}
 }
 
 function closeImg(){
 	fulImgBox.style.display = "none";
-	x.style.display = "none"
 	fulImg.classList.replace("img_ampliada", "img_original");
 	fulImgBox.classList.replace("ful_img_ampliado", "ful_img");
 	imgContainer.classList.replace("img_container_ampliado", "img_container");
+	x.style.display = "none";
+	modalButtons.forEach(button =>{
+		button.style.display = "none";
+	})
 	contador = 0;
 	enableScroll();
 }
@@ -72,6 +75,58 @@ function disableScroll(){
 function enableScroll(){
     body.style.removeProperty("overflow");
     imgContainer.style.removeProperty("overflow-y");
+}
+
+
+
+// ANTERIOR Y SIGUIENTE IMÁGEN
+
+function nextPrevControler(e){
+
+	if(e.target.id == "arrowRight"){
+
+		let sigImg;
+
+		if(fulImg.src == imagenes[imagenes.length-1].src){
+			sigImg = 0
+		}
+		else{
+			for(let i = 0; i < imagenes.length; i++){
+				if(fulImg.src == imagenes[i].src){
+					sigImg = i + 1;
+				}
+			}
+		}
+
+		fulImg.src = imagenes[sigImg].src;
+
+	}
+	else if(e.target.id == "arrowLeft"){
+
+		let prevImg;
+
+		if(fulImg.src == imagenes[0].src){
+			prevImg = imagenes.length - 1;
+		}
+		else{
+			for(let i = 0; i < imagenes.length; i++){
+				if(fulImg.src == imagenes[i].src){
+					prevImg = i - 1;
+				}
+			}
+		}
+
+		fulImg.src = imagenes[prevImg].src;
+
+	}
+
+	if(fulImg.classList.contains("img_ampliada")){
+		fulImg.classList.replace("img_ampliada", "img_original");
+		fulImgBox.classList.replace("ful_img_ampliado", "ful_img");
+		imgContainer.classList.replace("img_container_ampliado", "img_container");
+		contador = 0
+	}
+
 }
 
 
@@ -98,12 +153,16 @@ fulImg.addEventListener("click",fulImgZoom,true);
 
 imgContainer.addEventListener("click",fulImgClose);
 
-x.addEventListener("click",closeImg)
+x.addEventListener("click",closeImg);
+
+modalButtons.forEach(button =>{
+	button.addEventListener("click",nextPrevControler)
+})
 
 document.addEventListener("click", (e)=>{
 	const imagen = e.target;
-	if(imagen.className === "grid-item__item"){
-		return openFulImg(imagen.src);
+	if(imagen.className === "grid_item__mask"){
+		return openFulImg(imagen.parentNode.childNodes[1].src);
 	}
 })
 
